8a489250ebab20fa473fb6b23b93f28d1480e31d
Justian <just.tzh@outlook.com>
2022-12-19 23:17:46 +0800

    修改Frame类型的设计

--- include/Just/Math/Frame.h
+++ include/Just/Math/Frame.h
@@ -12,40 +12,25 @@ struct Frame
     Frame(const Vector3f &s, const Vector3f &t, const Vector3f &n) : s(s), t(t), n(n) {}
     Vector3f ToLocal(const Vector3f &v) const { return {Dot(v, s), Dot(v, t), Dot(v, n)}; }
     Vector3f ToWorld(const Vector3f &v) const { return s * v.x + t * v.y + n * v.z; }
+    static float AbsCosTheta(const Vector3f &v) { return std::abs(v.z); }
+    static float CosTheta(const Vector3f &v) { return v.z; }
+    static float Cos2Theta(const Vector3f &v) { return v.z * v.z; }
+    //注意避免因浮点数误差导致的极小可能的负值
+    static float Sin2Theta(const Vector3f &v) { return std::max(0.0f, 1.0f - Cos2Theta(v)); }
+    static float SinTheta(const Vector3f &v) { return std::sqrt(Sin2Theta(v)); }
+    static float TanTheta(const Vector3f &v) { return SinTheta(v) / CosTheta(v); }
+    static float Tan2Theta(const Vector3f &v) { return Sin2Theta(v) / Cos2Theta(v); }
+    static float CosPhi(const Vector3f &v)
+    {
+        float sinTheta = SinTheta(v);
+        return (sinTheta == 0) ? 1 : std::clamp(v.x / sinTheta, -1.0f, 1.0f);
+    }
+    static float SinPhi(const Vector3f &v)
+    {
+        float sinTheta = SinTheta(v);
+        return (sinTheta == 0) ? 0 : std::clamp(v.y / sinTheta, -1.0f, 1.0f);
+    }
+    static float Cos2Phi(const Vector3f &v) { return CosPhi(v) * CosPhi(v); }
+    static float Sin2Phi(const Vector3f &v) { return SinPhi(v) * SinPhi(v); }
 };
 
-inline float AbsCosTheta(const Vector3f &v) { return std::abs(v.z); }
-inline float CosTheta(const Vector3f &v) { return v.z; }
-inline float Cos2Theta(const Vector3f &v) { return v.z * v.z; }
-//注意避免因浮点数误差导致的极小可能的负值
-inline float Sin2Theta(const Vector3f &v) { return std::max(0.0f, 1.0f - Cos2Theta(v)); }
-inline float SinTheta(const Vector3f &v)
-{
-    return std::sqrt(Sin2Theta(v));
-}
-inline float TanTheta(const Vector3f &v)
-{
-    return SinTheta(v) / CosTheta(v);
-}
-inline float Tan2Theta(const Vector3f &v)
-{
-    return Sin2Theta(v) / Cos2Theta(v);
-}
-inline float CosPhi(const Vector3f &v)
-{
-    float sinTheta = SinTheta(v);
-    return (sinTheta == 0) ? 1 : std::clamp(v.x / sinTheta, -1.0f, 1.0f);
-}
-inline float SinPhi(const Vector3f &v)
-{
-    float sinTheta = SinTheta(v);
-    return (sinTheta == 0) ? 0 : std::clamp(v.y / sinTheta, -1.0f, 1.0f);
-}
-inline float Cos2Phi(const Vector3f &v)
-{
-    return CosPhi(v) * CosPhi(v);
-}
-inline float Sin2Phi(const Vector3f &v)
-{
-    return SinPhi(v) * SinPhi(v);
-}
